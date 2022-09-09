d64f8c5bd37fcd19999a95a9f14efa7b85cf55d8
DominickDiaz <74343440+DominickDiaz@users.noreply.github.com>
2022-09-08 21:26:02 -0400

    first commit

--- api/request.go
+++ api/request.go
@@ -3,24 +3,26 @@ package api
 import (
 	"MarsWeatherApp/database"
 	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func StartAPI() {
-	client := database.Connection()								// Fetch database connection/client
-	app := fiber.New()											// Creates fiber instance for api					
+	port := os.Getenv("PORT")
+	client := database.Connection() // Fetch database connection/client
+	app := fiber.New()              // Creates fiber instance for api
 	// GET /api/register
 
-
-	app.Get("/currentdata", func(c *fiber.Ctx) error {			// Endpoint for most recent information in database
-		msg := client.RetreiveData()		 			 	    // Retrieve data from database using mongoDB drivers
+	app.Get("/currentdata", func(c *fiber.Ctx) error { // Endpoint for most recent information in database
+		msg := client.RetreiveData() // Retrieve data from database using mongoDB drivers
 		//result := make(map[string][]nasa_api.Soles)
 		// result["data"] = msg
-		newData, _ := (json.Marshal(msg))						// method for converting a go struct into json
-		return c.SendString(string(newData)) 					// Sends json object 
+		newData, _ := (json.Marshal(msg))    // method for converting a go struct into json
+		return c.SendString(string(newData)) // Sends json object
 	})
-
-
 
 	// app.Post("/todos", func(c *fiber.Ctx) error {
 	// 	date := c.Query("")
@@ -28,5 +30,6 @@ func StartAPI() {
 	// 	return c.SendString("Request Successfully Posted")
 	// })
 
-	app.Listen(":9009")			// port number for api
+	log.Fatal(app.Listen(fmt.Sprintf(":%s", port)))
+	// port number for api
 }
